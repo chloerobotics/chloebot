468dc1d18a8a9695c9383996fcef5bc891774936
carson <you@example.com>
2019-11-16 15:11:26 -0800

    fixed positional encoder

--- scripts/Transformer.py
+++ scripts/Transformer.py
@@ -22,18 +22,21 @@ class PositionalEncoder(nn.Module):
         pe = torch.zeros(max_seq_len, d_model)
         for pos in range(max_seq_len):
             for i in range(0, d_model, 2):
+                
                 pe[pos, i] = math.sin(pos / (10000 ** ((2 * i)/d_model)))
-                pe[pos, i + 1] = math.cos(pos / (10000 ** ((2 * (i + 1))/d_model)))
-        pe = pe.unsqueeze(0)
+                pe[pos, i + 1] = math.cos(pos / (10000 ** ((2 * i)/d_model)))
+        pe = pe.unsqueeze(0) # add a batch dimention to your pe matrix 
         self.register_buffer('pe', pe)
  
     def forward(self, x):
-        print('x.shape', x.shape)
+        
         # make embeddings relatively larger
         x = x * math.sqrt(self.d_model)
         #add constant to embedding
         seq_len = x.size(1)
         pe = Variable(self.pe[:,:seq_len], requires_grad=False)
+        #print('x.shape', x.shape) # (batch_size, input_seq_len, d_model)
+        #print('pe.shape', pe.shape) # (1, input_seq_len, d_model)
         if x.is_cuda:
             pe.cuda()
         x = x + pe
